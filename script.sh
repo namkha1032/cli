c00392eb99c5fa1d57a347573a2410badbf7c29f
Nam Kha Nguyen <113871587+namkha1032@users.noreply.github.com>
2025-08-15 11:38:42 +0930

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
 #SBATCH -p a100
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --time=01:00:00
-#SBATCH --mem=4GB  
+#SBATCH --time=00:05:00
+#SBATCH --mem=4GB
+#SBATCH --gpus-per-node=1
+
+#SBATCH --output=slurm-%j.log
+#SBATCH --error=slurm-%j.log
 
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=namkha.nguyen@adelaide.edu.au
